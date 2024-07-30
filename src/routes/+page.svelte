ec6b7bee16e547634b0c9f5c51f67f963bb5418c
Daschi <daschi.03@icloud.com>
2024-07-31 00:55:24 +0200

    add basic functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import Coordinate from "./Coordinate.svelte";
+
+  const westStart = 56;
+  const westEnd = 20;
+  const westLength = westStart - westEnd + 1; // 36 + 1
+  $inspect(westLength);
+
+  const southStart = 12;
+  const southEnd = 50;
+  const southLength = southEnd - southStart + 1; // 38 + 1
+</script>
+
+<div class="relative w-[2751px] h-[2899px] cursor-crosshair">
+  <img class="pointer-events-none" alt="soa-map.png" src="soa-map.png">
+
+  <div
+    class="absolute top-0 left-0 w-full h-full grid grid-cols-[repeat(37,_minmax(0,_1fr))] grid-rows-[repeat(39,_minmax(0,_1fr))]">
+    {#each { length: southLength } as _, south}
+      {#each { length: westLength } as _, west}
+        <Coordinate south={southStart + south} west={westStart - west} />
+      {/each}
+    {/each}
+  </div>
+</div>
---
+++ src/routes/Coordinate.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  interface Props {
+    west: number;
+    south: number;
+  }
+
+  let { west, south }: Props = $props();
+
+  let visible = $state(false);
+
+  function toggleVisibility() {
+    visible = !visible;
+  }
+</script>
+
+<div class="p-0.5 opacity-{visible ? 100 : 0} hover:opacity-100" aria-checked={visible}
+     onclick={toggleVisibility} onkeydown={() => {}}
+     role="checkbox" tabindex="0">
+  <div
+    class="w-full h-full bg-blue-500/75 text-xl text-black select-none flex flex-col items-center justify-center">
+    <div class="text-xl">
+      <span class="font-bold">{west}</span>'W
+    </div>
+    <div class="">
+      <span class="font-bold">{south}</span>'S
+    </div>
+  </div>
+</div>
