42632ee4af366b1fcc3e6f4af76f66b33dab60fd
Daschi <daschi.03@icloud.com>
2024-08-03 01:01:24 +0200

    extract into Map component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,5 @@
 <script lang="ts">
-  import Coordinate from "./Coordinate.svelte";
-
-  const westStart = 56;
-  const westEnd = 20;
-  const westLength = westStart - westEnd + 1; // 36 + 1
-
-  const southStart = 12;
-  const southEnd = 50;
-  const southLength = southEnd - southStart + 1; // 38 + 1
+  import Map from "$lib/Map.svelte";
 </script>
 
-<div class="relative h-[2899px] w-[2751px] cursor-crosshair">
-  <img class="pointer-events-none" alt="soa-map" src="soa-map-updated.png" />
-
-  <div
-    class="absolute left-0 top-0 grid h-full w-full grid-cols-[repeat(37,_minmax(0,_1fr))] grid-rows-[repeat(39,_minmax(0,_1fr))]"
-  >
-    <!-- eslint-disable-next-line @typescript-eslint/no-unused-vars -->
-    {#each { length: southLength } as _, south}
-      <!-- eslint-disable-next-line @typescript-eslint/no-unused-vars -->
-      {#each { length: westLength } as _, west}
-        <Coordinate south={southStart + south} west={westStart - west} />
-      {/each}
-    {/each}
-  </div>
-</div>
+<Map src="soa-map-updated.png" />
