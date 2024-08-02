67e76e609d67b2b4bbb641960ebb7ae319f4a29e
Daschi <daschi.03@icloud.com>
2024-08-03 01:20:52 +0200

    add svelte:head

--- src/routes/spirits-of-amazonia/+page.svelte
+++ src/routes/spirits-of-amazonia/+page.svelte
@@ -2,4 +2,9 @@
   import Map from "$lib/Map.svelte";
 </script>
 
+<svelte:head>
+  <title>Green Hell Map - Spirits of Amazonia</title>
+  <meta name="description" content="Green Hell interactive map for Spirits of Amazonia" />
+</svelte:head>
+
 <Map src="soa-map-updated.png" />
--- src/routes/story-mode/+page.svelte
+++ src/routes/story-mode/+page.svelte
@@ -2,4 +2,9 @@
   import Map from "$lib/Map.svelte";
 </script>
 
+<svelte:head>
+  <title>Green Hell Map - Story Mode</title>
+  <meta name="description" content="Green Hell interactive map for Story Mode" />
+</svelte:head>
+
 <Map src="sm-map-updated.png" />
