4017e33309892f0abe2335dcd25a01d361aa142d
Daschi <daschi.03@icloud.com>
2024-08-05 22:01:47 +0200

    extract Settings component

---
+++ src/lib/Settings.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { Checkbox, Label, Range } from "flowbite-svelte";
+  import { CloseOutline } from "flowbite-svelte-icons";
+  import { alwaysShowCoordinateOverlay, coordinateOverlayOpacity } from "$lib/settings";
+</script>
+
+<div class="flex flex-row items-center gap-2">
+  <Label>Coordinate overlay opacity:</Label>
+  <span>
+    <Range max={1} min={0} step={0.05} bind:value={$coordinateOverlayOpacity} />
+  </span>
+  <!-- TODO reset to default range -->
+  <Checkbox bind:checked={$alwaysShowCoordinateOverlay}>Always show coordinate overlay</Checkbox>
+  <!-- TODO unselect all coordinates -->
+  <CloseOutline id="unselectAllCoordinates" />
+  <Label for="unselectAllCoordinates">Unselect all coordinates</Label>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,27 +1,13 @@
 <script lang="ts">
   import "../app.css";
-  import { Checkbox, Label, Range } from "flowbite-svelte";
   import Navbar from "$lib/Navbar.svelte";
-  import { CloseOutline } from "flowbite-svelte-icons";
-  import { alwaysShowCoordinateOverlay, coordinateOverlayOpacity } from "$lib/settings";
+  import Settings from "$lib/Settings.svelte";
 </script>
 
 <div class="flex flex-col gap-2 p-2">
   <div class="flex flex-row items-center gap-4">
     <Navbar />
-    <div class="flex flex-row items-center gap-2">
-      <Label>Coordinate overlay opacity:</Label>
-      <span>
-        <Range max={1} min={0} step={0.05} bind:value={$coordinateOverlayOpacity} />
-      </span>
-      <!-- TODO reset to default range -->
-      <Checkbox bind:checked={$alwaysShowCoordinateOverlay}
-        >Always show coordinate overlay
-      </Checkbox>
-      <!-- TODO unselect all coordinates -->
-      <CloseOutline id="unselectAllCoordinates" />
-      <Label for="unselectAllCoordinates">Unselect all coordinates</Label>
-    </div>
+    <Settings />
   </div>
 
   <slot></slot>
