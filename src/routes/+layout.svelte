501ea5b1decbb2d005c2f48ee46266a2d4c84111
Daschi <daschi.03@icloud.com>
2024-08-05 22:46:51 +0200

    hide settings when not on a map page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,15 @@
   import "../app.css";
   import Navbar from "$lib/Navbar.svelte";
   import Settings from "$lib/Settings.svelte";
+  import { page } from "$app/stores";
 </script>
 
 <div class="flex flex-col gap-2 p-2">
   <div class="flex flex-row items-center gap-4">
     <Navbar />
-    <Settings />
+    {#if $page.url.pathname.length > 1}
+      <Settings />
+    {/if}
   </div>
 
   <slot></slot>
