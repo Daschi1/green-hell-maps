01d6a5edfcfa01a1f0d2d5953496addce30e5fb3
Daschi <daschi.03@icloud.com>
2024-08-05 21:58:36 +0200

    add items-center

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -6,7 +6,7 @@
   let activePathname = $derived($page.url.pathname);
 </script>
 
-<div class="flex flex-row gap-2">
+<div class="flex flex-row items-center gap-2">
   <NavbarA {activePathname} href="/{$page.url.search}"><Span>Green Hell Maps</Span></NavbarA>
   <NavbarA {activePathname} href="/story-mode{$page.url.search}">Story Mode</NavbarA>
   <NavbarA {activePathname} href="/spirits-of-amazonia{$page.url.search}"
